c716017c2c1b63fbe7ddb98a8c76ee4600adc254
Kostia Hryhorov <kgraven2009@gmail.com>
2025-12-02 11:14:28 +0200

    видалено рядок

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 print("Вітаю в грі 'Вгадай число'!")
 player_name = input("Введи свій нікнейм: ")
-print("Привіт, {player_name}! Почнемо гру!")
+print(f"Привіт, {player_name}! Почнемо гру!")
 
 attempts_history = []
 
@@ -25,8 +25,8 @@ while True:
             elif guess > secret_number:
                 print("Моє число менше!")
             else:
-                print("Вітаю, {player_name}! Ти вгадав число {secret_number}!")
-                print("Кількість спроб: {attempts}")
+                print(f"Вітаю, {player_name}! Ти вгадав число {secret_number}!")
+                print(f"Кількість спроб: {attempts}")
                 attempts_history.append(attempts)
                 break
         else:
